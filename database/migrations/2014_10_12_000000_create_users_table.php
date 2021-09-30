f5d8aed08b33f0137ecf94c897ca3518dc67b75f
= <=>
2021-09-30 14:37:40 +0200

    Made first migration user table.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,24 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
             $table->string('password');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('country');
+            $table->string('province');
+            $table->date('date_of_birth');
+            $table->string('gender');
+            $table->string('sexual_orientation');
+            $table->string('preference');
+            $table->string('university');
+            $table->string('passions');
+            $table->string('user_picture');
+            $table->string('about_me');
+            $table->string('likes');
+            $table->string('likes_given');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
